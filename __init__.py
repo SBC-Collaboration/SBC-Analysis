da9cc9353516dc7157c73645305be68e4046bcdf
John Gresl <j.gresl12@gmail.com>
2018-08-27 15:50:39 -0500

    Removed old function imports.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,7 @@
 from .DataHandling.ReadBinary import ReadBlock as read_bin
 from .DataHandling.ReadText import ReadFile as read_txt
 from .DataHandling.GetSBCEvent import GetEvent as get_event
-from .AnalysisModules.EventDealer import ProcessSingleRun as psr
 from .AnalysisModules.EventDealer import ProcessSingleRun2 as psr2
-from .AnalysisModules.EventDealer import ProcessSingleRun_pmtfdaonly as psr_pmtfda
-from .AnalysisModules.EventDealer import ProcessSingleRun_historyonly as psr_ha
-from .AnalysisModules.EventDealer import ProcessSingleRun_phecountingonly as psr_pmtphea
-from .AnalysisModules.EventDealer import ProcessSingleRun_pmtpfonly as psr_pmtpf
 __all__ = ["DataHandling"]
 
 
