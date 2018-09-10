d48c5f3fb4d4c81e64b772c070b9eb0e3f8bf1df
John Gresl <j.gresl12@gmail.com>
2018-09-10 11:29:51 -0500

    SiPM testing script. Noticing too much noise right now

--- UserCode/John/SiPMTests.py
+++ UserCode/John/SiPMTests.py
@@ -13,15 +13,20 @@ class SiPMTrigger(object):
         self.trig=trig
 
 class SiPMPlotter(object):
-    def __init__(self, pmt_data, default_trigger=0, runid=None, ev=None):
+    def __init__(self, pmt_data, left=None, right=None,
+                 default_trigger=0, runid=None, ev=None, areas=None):
         self.pmt_data = pmt_data
         self.default_trigger = default_trigger
         self.trigger = SiPMTrigger(trig=self.default_trigger)
         self.fig = plt.figure()
         self.runid = runid
         self.ev = ev
+        self.areas=areas
+        self.left = left
+        self.right = right
         self.update_title()
         cid = plt.gcf().canvas.mpl_connect("key_press_event", self.key_press)
+
         return
 
     def plot_SiPM_trace(self):
@@ -33,6 +38,8 @@ class SiPMPlotter(object):
                   pmt_data["v_offset"][self.trigger.trig, 1]
         plt.plot(xd, yd_0)
         plt.plot(xd, yd_1)
+        plt.axvline(self.left, color="k", linewidth=2)
+        plt.axvline(self.right, color="k", linewidth=2)
         dt = pmt_data["dt"][self.trigger.trig, 0]
         #plt.specgram(yd_1, NFFT=256, Fs=1e-9 /dt, )
         self.update_title()
@@ -50,8 +57,8 @@ class SiPMPlotter(object):
         return
 
     def update_title(self):
-        plt.suptitle("Runid: {} || Event: {} || Trigger {}\nUse Left and Right arrows to navigate."\
-                     .format(self.runid, self.ev, self.trigger.trig))
+        plt.suptitle("Runid: {} || Event: {} || Trigger {}\nUse Left and Right arrows to navigate.\nArea={}"\
+                     .format(self.runid, self.ev, self.trigger.trig, self.areas[self.trigger.trig]))
 
 if __name__ == "__main__":
     raw_directory = r"C:\Users\John\Documents\SBC-18-data"
@@ -84,6 +91,8 @@ if __name__ == "__main__":
     max_times = []
     n_runs =  len(var_array)
     plt.ioff()
+    left_lim = 700
+    right_lim = 1300
     for run_ix in range(n_runs):
         sub_areas = []
         sub_max_times = []
@@ -98,7 +107,7 @@ if __name__ == "__main__":
                        pmt_data["v_offset"][trig, 0]
                 yd_1 = pmt_data["traces"][trig, 1, :] * pmt_data["v_scale"][trig, 1] + \
                        pmt_data["v_offset"][trig, 1]
-                good_indices = (xd < 1800) & (xd > 700)
+                good_indices = (xd < right_lim) & (xd > left_lim)
 
                 pmt_area = scipy.integrate.trapz(yd_0[good_indices],
                                                  dx=1e9*pmt_data["dt"][trig, 1])
@@ -106,17 +115,20 @@ if __name__ == "__main__":
                 sub_areas.append(pmt_area)
                 sub_max_times.append(1e9 * pmt_data["dt"][trig, 0] * np.argmax(pmt_data["traces"][trig, 0, :]))
                 pass
-            plotter = SiPMPlotter(pmt_data, runid=os.path.basename(active_event), ev=ev)
+            plotter = SiPMPlotter(pmt_data, runid=os.path.basename(active_event), ev=ev,
+                                  areas=sub_areas,
+                                  left=left_lim, right=right_lim)
             plotter.plot_SiPM_trace()
+
             plt.show()
         areas.append(sub_areas)
         max_times.append(sub_max_times)
 
-    plt.hist(areas, bins=3600, fill=False, color=colors, histtype="step", stacked=False)
+    plt.hist(areas, bins=3000, fill=False, color=colors, histtype="step", stacked=False)
     plt.legend(labels)
     plt.suptitle("Areas")
     plt.figure()
-    plt.hist(max_times, bins=3600, fill=False, color=colors, histtype="step", stacked=False)
+    plt.hist(max_times, bins=3000, fill=False, color=colors, histtype="step", stacked=False)
     plt.legend(labels)
     plt.suptitle("Time of max")
     plt.show()
