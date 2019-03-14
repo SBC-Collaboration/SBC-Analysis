f6064f0c02a98d2149584102a2f2bec865601d68
Matt <mjb536@drexel.edu>
2019-03-14 17:48:55 -0500

    More work on PMT response

--- UserCode/bressler/PMTresponsetoLEDs.py
+++ UserCode/bressler/PMTresponsetoLEDs.py
@@ -6,30 +6,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 
-runpath = "/bluearc/storage/SBC-17-data/20170718_4/"
+runpath = "/bluearc/storage/SBC-17-data/20170706_4/"
 events = [evnt for evnt in listdir(runpath) if not isfile(join(runpath,evnt))]
 
 e = sbc.DataHandling.GetSBCEvent.GetEvent(runpath,0)
-d=sbc.AnalysisModules.PMTfastDAQalignment.PMTandFastDAQalignment(e)
-print(d.keys())
+
+#d=sbc.AnalysisModules.PMTfastDAQalignment.PMTandFastDAQalignment(e)
+#print(d.keys())
 tr = e["PMTtraces"]
 trac = tr["traces"]
 t0 = tr["t0"]
 dt = tr["dt"]
+pmttrig = e["fastDAQ"]["PMTtrig"]
 print(tr.keys())
-print(t0[0])
+
+x=np.arange(len(othertrace))
+plt.figure()
+plt.plot(x,othertrace)
+plt.plot(x,trace)
+plt.show
 
 V = []
+VwithNIM = []
 indices_of_high = []
 for i in range(len(trac)):
     trace = trac[i][0]
+    othertrace = trac[i][1]
     V.append(np.fabs(min(trace)))
     
     if np.fabs(min(trace)) == 128:
         indices_of_high.append(i)
+    if min(othertrace) < -30:
+        VwithNIM.append(np.fabs(min(trace)))
 plt.figure()
-plt.hist(np.asarray(V),100)
+plt.hist(np.asarray(V),80,color='r',histtype = 'step')
+plt.hist(np.asarray(VwithNIM),80,color='b',histtype='step')
+plt.title('RunType 902: 20170706_4, event 0')
+plt.xlabel('V max')
 plt.show
+
+print(len(indices_of_high))
 
 for j in range(len(indices_of_high)):
     trace = np.fabs(trac[indices_of_high[j]][0])
