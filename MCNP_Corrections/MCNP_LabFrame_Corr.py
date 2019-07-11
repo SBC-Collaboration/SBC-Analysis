463c0a171d569c060ba20bbc1e56e58b30cf5523
C Eric Dahl <cdahl@mugiwara2.phys.northwestern.edu>
2019-07-11 14:10:32 -0500

    Added known gamma and Q-value energies

--- MCNP_Corrections/MCNP_LabFrame_Corr.py
+++ MCNP_Corrections/MCNP_LabFrame_Corr.py
@@ -10,19 +10,46 @@ Created on Tue Sep  1 16:07:15 2018
 import numpy as np
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 # Lorentz Boost with Momentum and Theta inputs -- all in keV
-def boost(p_photon, theta, mass_target = 9.012183078): # reaction-dependent
-    amu = 931494; # keV/c^2 per amu, per physics.bu.edu
-    mass_neutron = 1008664.91585; # listed in "keV/c^2" 1008664.91585
-    Q = 1661; # Be9_mass - Be8_mass; # listed in keV # reaction-dependent
-    
+def boost(p_photon, cos_theta, Qval, mass_target = np.float64(9.01218307)): # reaction-dependent
+    amu = np.float64(931494.0954) # keV/c^2 per amu, per physics.bu.edu
+    mass_neutron = np.float64(1.0086649158) # amu
+    known_Qvals = np.float64([ # keV
+                              1664.5371, # 9Be
+                              ])
+    known_targets = np.float64([ # amu
+                                9.01218307, # 9Be
+                                ])
+    known_gammas = np.float64([ # keV
+                               1690.971, # 124Sb
+                               2090.930, # 124Sb
+                               1770.228, # 207Bi
+                               1836.063, # 88Y
+                               2734.0, # 88Y
+                               1674.725, # 58Co
+                               ])
+
+    # Identify target by Q value
+    Qdiff = np.abs(known_Qvals - Qval)
+    target_ix = np.argmin(Qdiff)
+    if Qdiff[target_ix] < 1:
+        Qval = known_Qvals[target_ix]
+        mass_target = known_targets[target_ix]
+
     # Masses
     M = mass_target*amu;
-    m_1 = mass_neutron;
-    m_2 = M - m_1 + Q;
+    m_1 = mass_neutron*amu;
+    m_2 = M - m_1 + Qval;
+
+    # Identify photon line
+    photon_Ediff = np.abs(known_gammas - p_photon)
+    photon_ix = np.argmin(photon_Ediff)
+    if photon_Ediff[photon_ix] < 1:
+        p_photon = known_gammas[photon_ix]
 
     # Rotational Lorentz "phi"
     phi = .5*np.log((2*p_photon/M) + 1);
     
+    """
     # Lorentz Boost Matrix  
     dim = 4
     Lambda = [[0 for x in range(dim)] for y in range(dim)];
@@ -64,8 +91,33 @@ def boost(p_photon, theta, mass_target = 9.012183078): # reaction-dependent
     # alpha (deflection angle in lab frame) calculation
     spatial_p = np.array([[p_lab[1]],[p_lab[2]],[p_lab[3]]])
     alpha = np.arctan2(spatial_p[1],spatial_p[0]);
-    
-    return float(recoil_energy), float(alpha)
+    """
+
+    Ep_com = p_photon * np.sqrt(M/(2*p_photon+M)) # photon energy in com frame
+    EM_com = np.sqrt(M*M + Ep_com*Ep_com) # target nucleus energy in com frame
+    p2_com = ( # |p|^2 for either particle in com frame
+              Ep_com**4 + (Ep_com**2 - 0.5*Qval**2)**2 +
+              2*Qval*(M-m_1)*(.5*Qval**2 - Ep_com**2 - m_1*M) +
+              2*Ep_com*EM_com*(Ep_com**2 + (M-m_1)*(m_1-Qval) - .5*Qval**2) +
+              Ep_com**2 * (M**2 - 2*m_1**2 + 2*M*m_1) +
+              Qval**2 * (M**2 + m_1**2 - 3*m_1*M)) / ((Ep_com + EM_com)**2)
+    p_com = np.zeros(3, dtype=np.float64) # 4-momentum of neutron in com frame, 4th element=0
+    p_com[0] = np.sqrt(m_1**2 + p2_com)
+    p_com[1] = np.sqrt(p2_com)*cos_theta
+    sin_theta = np.sqrt(1 - cos_theta**2)
+    p_com[2] = np.sqrt(p2_com)*sin_theta
+
+    p = np.zeros(3, dtype=np.float64) # 4-momentum of neutron in lab frame, 4th element=0
+    p[0] = p_com[0]*np.cosh(phi) + p_com[1]*np.sinh(phi)
+    p[1] = p_com[1]*np.cosh(phi) + p_com[0]*np.sinh(phi)
+    p[2] = p_com[2]
+
+    p2 = p[1]**2 + p[2]**2 # |p|^2 of neutron in lab frame
+
+    recoil_energy = (.5 * p2 / m_1) * (1 - .25 * p2/(m_1**2)) # 1st order rel correction
+
+    recoil_costheta = p[1] / np.sqrt(p2)
+    return recoil_energy, recoil_costheta
 
 # End of Momentum Correction Function
 
@@ -94,22 +146,23 @@ def corrections_MCNP(input_file, output_file):
                 
                 if index % 3 == 1:
                     # no corrections necessary   
-                    input_energy = 1000*float(line[2:9])
+                    input_energy = 1000*np.float64(line[2:9])
+                    input_Qval = -1000*np.float64(line[20:29])
                     g.write(line)
                 
                 if index % 3 == 2:
                     # corrections definitely necessary
                     g.write(line[:5])
 
-                    cos_theta = float(line[37:45])
-                    CoM_theta = np.arccos(cos_theta)
-                    lab_re, lab_alpha = boost(input_energy,CoM_theta)
+                    cos_theta = np.float64(line[37:45])
+                    # CoM_theta = np.arccos(cos_theta)
+                    lab_re, cos_alpha = boost(input_energy, cos_theta, input_Qval)
  
-                    g.write(str(round(lab_re/1000, 3)))
+                    g.write(str(round(lab_re/1000, 6)))
                 
                     g.write(line[10:37])
                 
-                    cos_alpha = np.cos(lab_alpha)
+                    # cos_alpha = np.cos(lab_alpha)
                 
                     if cos_alpha >= 0:
                         g.write(" " + str(round(cos_alpha, 5)))
