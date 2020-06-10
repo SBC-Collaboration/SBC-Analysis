c86a891cfa54a62ae38ddad5b2be95577ed2845f
shishirb <shishir.bandapalli@hotmail.com>
2020-06-10 13:09:54 -0500

    Added video functionality and revised capture

--- DAQ/Cameras/RPi_CameraServers/python/capture.py
+++ DAQ/Cameras/RPi_CameraServers/python/capture.py
@@ -2,11 +2,9 @@
 #file is labelled with data and time when taken
 
 import arducam_mipicamera as arducam
-#import v4l2
-import time
+import v4l2
 from datetime import datetime
-import numpy as np
-import os
+
 
 def set_controls(camera):
     try:
@@ -14,29 +12,29 @@ def set_controls(camera):
         camera.software_auto_exposure(enable = True)
     except Exception as e:
         print(e)
-
+def capture(camera):
+    frame = camera.capture(encoding = 'jpeg')
+    d1 = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
+    path = "/home/pi/SBCcode/DAQ/Cameras/RPi_CameraServers/python/Captures/"+d1+".jpg"
+    open(path, "wb")
+    frame.as_array.tofile(path)
+            #Remove frame from memory
+    del frame
+    
+            
 if __name__ == "__main__":
-    try:
-        camera = arducam.mipi_camera()
-        print("Open camera...")
-        camera.init_camera()
-        print("Setting the resolution...")
-        fmt = camera.set_resolution(1920, 1080)
-        print("Current resolution is {}".format(fmt))
-        set_controls(camera)
-        frame = camera.capture(encoding = 'jpeg')
-        d1 = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
-        path = "/home/pi/SBCcode/DAQ/Cameras/RPi_CameraServers/python/Captures/"+d1+".jpg"
-        open(path, "wb")
-        frame.as_array.tofile(path)
-        
-        
-        
-       
-
-        # Release memory
-        del frame
-        print("Close camera...")
-        camera.close_camera()
-    except Exception as e:
-        print(e)
+        try:
+            camera = arducam.mipi_camera()
+            print("Open camera...")
+            camera.init_camera()
+            print("Setting the resolution...")
+            fmt = camera.set_resolution(1920, 1080)
+            camera.set_control(v4l2.V4L2_CID_VFLIP, 1)
+            camera.set_control(v4l2.V4L2_CID_HFLIP,1)
+            print("Current resolution is {}".format(fmt))
+            set_controls(camera)
+            capture(camera)
+            print("Close camera...")
+            camera.close_camera()
+        except Exception as e:
+            print(e)
--- DAQ/Cameras/RPi_CameraServers/python/video.py
+++ DAQ/Cameras/RPi_CameraServers/python/video.py
@@ -3,20 +3,16 @@ import v4l2 #sudo pip install v4l2
 import time
 import ctypes
 import numpy as np
+from datetime import datetime
+import os
+from capture import capture
 
 def set_controls(camera):
     try:
-        print("Reset the focus...")
-        camera.reset_control(v4l2.V4L2_CID_FOCUS_ABSOLUTE)
-    except Exception as e:
-        print(e)
-        print("The camera may not support this control.")
-    try:
         print("Enable Auto Exposure...")
-        camera.software_auto_exposure(enable = True)
+        #camera.software_auto_exposure(enable = True)
         time.sleep(2)
-        print("Enable Auto White Balance...")
-        camera.software_auto_white_balance(enable = True)
+        
     except Exception as e:
         print(e)
 
@@ -25,7 +21,6 @@ def callback(data):
     file = buff.userdata
     buff.as_array.tofile(file)
     return 0
-
 if __name__ == "__main__":
     try:
         camera = arducam.mipi_camera()
@@ -34,14 +29,33 @@ if __name__ == "__main__":
         print("Setting the resolution...")
         fmt = camera.set_resolution(1920, 1080)
         print("Current resolution is {}".format(fmt))
+        camera.set_control(v4l2.V4L2_CID_VFLIP, 1)
+        camera.set_control(v4l2.V4L2_CID_HFLIP,1)
         print("Start preview...")
         camera.start_preview(fullscreen = False, window = (0, 0, 1280, 720))
+        preview = True
         set_controls(camera)
-        file = open("test.h264", "wb")
+        cont = True
+        d1 = datetime.now().strftime("%m-%d-%Y_%H:%M:%S")
+        path = "/home/pi/SBCcode/DAQ/Cameras/RPi_CameraServers/python/Video/"+d1+".h264"
+        file = open(path, "wb")
         # Need keep py_object reference
         file_obj = ctypes.py_object(file)
         camera.set_video_callback(callback, file_obj)
-        time.sleep(10)
+        while cont:
+            cmd = input()
+            cmd = cmd.lower()
+            if (cmd == "s"):
+                camera.stop_preview()
+                preview = False
+            elif(cmd=="p" and not preview):
+                camera.start_preview(fullscreen=False, window = (0,0,1280,720))
+                preview = True
+            elif (cmd == "c"):
+                capture(camera)
+            elif(cmd == "stop"):
+                cont = False
+        
         camera.set_video_callback(None, None)
         file.close()
         print("Stop preview...")
