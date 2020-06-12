7fff0d3d75cb3c3bf43432843fdb4eba7d2bd2df
shishirb <shishir.bandapalli@hotmail.com>
2020-06-12 14:24:47 -0500

    Added functionality for saving streamed stills as PNGs

--- DAQ/Cameras/RPi_CameraServers/python/stream.py
+++ DAQ/Cameras/RPi_CameraServers/python/stream.py
@@ -3,7 +3,7 @@ import arducam_mipicamera as arducam
 import v4l2
 from datetime import datetime
 import numpy as np
-#import keyboard
+from PIL import Image
 
 def set_controls(camera):
     try:
@@ -12,34 +12,45 @@ def set_controls(camera):
         print(e)
         
 if __name__ == "__main__":
-    camera = arducam.mipi_camera()
-    camera.init_camera()
-    print("camera open")
-    camera.set_resolution(1920,1080)
-    print("res set")
-    camera.set_control(v4l2.V4L2_CID_VFLIP, 1)
-    camera.set_control(v4l2.V4L2_CID_HFLIP,1)
-    set_controls(camera)
-    ringBuf = (np.zeros((1280,800)),) *100
-    i = 0
-    cont= True
-    while(cont):
-#        if keyboard.is_pressed("q"):
-#            cont=False
-        if(i==100):
-            # i = 0
-            break
-        else:
-            buff = ringBuf[i]
-            buff = camera.capture(encoding= "raw").as_array.reshape(1280,800)
-            print("capture")
-        i = i+1
-    camera.close_camera()
-    print("camera close")
-    for i in range(100):
-        open("/home/pi/SBCcode/DAQ/Cameras/RPi_CameraServers/python/Captures/"+str(i)+".raw","wb")
-        ringBuf[i].tofile("/home/pi/SBCcode/DAQ/Cameras/RPi_CameraServers/python/Captures/"+str(i)+".raw")
-        
+    try:
+        camera = arducam.mipi_camera()
+        camera.init_camera()
+        print("camera open")
+        camera.set_resolution(1920,1080)
+        print("res set")
+        camera.set_control(v4l2.V4L2_CID_VFLIP, 1)
+        camera.set_control(v4l2.V4L2_CID_HFLIP,1)
+        set_controls(camera)
+        ls = [None]*100
+        for i in range(100):
+            ls[i] = np.zeros((800,1280))
+        ringBuf = tuple(ls)
     
-        
-
+        i = 0
+        cont= True
+        while(cont):
+            
+            try:
+                if(i==100):
+                    break
+                else:
+                    frame = camera.capture(encoding="raw")
+                    buff1 = ringBuf[i]
+                    buff2= frame.as_array.reshape(800,1280)
+                    print("capture")
+                    np.copyto(buff1,buff2)
+                i = i+1
+            except KeyboardInterrupt:
+                break
+        camera.close_camera()
+        print("camera close")
+        im = Image.fromarray(ringBuf[1])
+        im = im.convert("L")
+        im.save("test.png")
+        for i in range(100):
+            im = Image.fromarray(ringBuf[i])
+            im = im.convert("L")
+            im.save("/home/pi/SBCcode/DAQ/Cameras/RPi_CameraServers/python/Captures/"+str(i)+".png")
+        print("images saved")
+    except KeyboardInterrupt:
+        print("ending image capture")
