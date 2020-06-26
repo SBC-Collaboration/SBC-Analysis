4091b6534fa1045d11e81f987d6672eb586b52e4
David V <velascodavid00@gmail.com>
2020-06-26 17:23:17 -0500

    Minor changes

--- DAQ/Cameras/RPi_CameraServers/python/core.py
+++ DAQ/Cameras/RPi_CameraServers/python/core.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jun 26 16:06:13 2020
-
 @author: dvelasco
 """
 
@@ -47,122 +46,122 @@ class Timer:
         print(f"Elapsed time: {elapsed_time:0.4f} seconds")
  
 #_____________________________________________________________________________  
-class DynamicArray(object): 
-    ''' 
-    DYNAMIC ARRAY CLASS (Similar to Python List) 
+class DynamicArray(object):
     '''
-      
-    def __init__(self): 
-        self.n = 0 # Count actual elements (Default is 0) 
-        self.capacity = 1 # Default Capacity 
-        self.A = self.make_array(self.capacity) 
-          
-    def __len__(self): 
-        """ 
-        Return number of elements sorted in array 
+    DYNAMIC ARRAY CLASS (Similar to Python List)
+    '''
+     
+    def __init__(self):
+        self.n = 0 # Count actual elements (Default is 0)
+        self.capacity = 1 # Default Capacity
+        self.A = self.make_array(self.capacity)
+         
+    def __len__(self):
         """
-        return self.n 
-      
-    def __getitem__(self, k): 
-        """ 
-        Return element at index k 
+        Return number of elements sorted in array
         """
-        if not 0 <= k <self.n: 
-            # Check it k index is in bounds of array 
+        return self.n
+     
+    def __getitem__(self, k):
+        """
+        Return element at index k
+        """
+        if not 0 <= k <self.n:
+            # Check it k index is in bounds of array
             return IndexError('K is out of bounds !')  
-          
-        return self.A[k] # Retrieve from the array at index k 
-          
-    def append(self, ele): 
-        """ 
-        Add element to end of the array 
+         
+        return self.A[k] # Retrieve from the array at index k
+         
+    def append(self, ele):
         """
-        if self.n == self.capacity: 
-            # Double capacity if not enough room 
+        Add element to end of the array
+        """
+        if self.n == self.capacity:
+            # Double capacity if not enough room
             self._resize(2 * self.capacity)  
-          
-        self.A[self.n] = ele # Set self.n index to element 
+         
+        self.A[self.n] = ele # Set self.n index to element
         self.n += 1
-    
-    def insertAt(self,item,index): 
-        """ 
-         This function inserts the item at any specified index. 
+   
+    def insertAt(self,item,index):
         """
-  
-          
-        if index<0 or index>self.n: 
-            print("please enter appropriate index..") 
+         This function inserts the item at any specified index.
+        """
+ 
+         
+        if index<0 or index>self.n:
+            print("please enter appropriate index..")
             return
-          
-        if self.n==self.capacity: 
-            self._resize(2*self.capacity) 
-              
-          
-        for i in range(self.n-1,index-1,-1): 
-            self.A[i+1]=self.A[i] 
-              
-          
-        self.A[index]=item 
+         
+        if self.n==self.capacity:
+            self._resize(2*self.capacity)
+             
+         
+        for i in range(self.n-1,index-1,-1):
+            self.A[i+1]=self.A[i]
+             
+         
+        self.A[index]=item
         self.n+=1
-  
-  
-          
-    def delete(self): 
-        """ 
-        This function deletes item from the end of array 
+ 
+ 
+         
+    def delete(self):
         """
-  
-        if self.n==0: 
-            print("Array is empty deletion not Possible") 
+        This function deletes item from the end of array
+        """
+ 
+        if self.n==0:
+            print("Array is empty deletion not Possible")
             return
-          
+         
         self.A[self.n-1]=0
         self.n-=1
-          
-          
-          
-      
-    def removeAt(self,index): 
-        """ 
-        This function deletes item from a specified index.. 
+         
+         
+         
+     
+    def removeAt(self,index):
+        """
+        This function deletes item from a specified index..
         """        
-  
-        if self.n==0: 
-            print("Array is empty deletion not Possible") 
+ 
+        if self.n==0:
+            print("Array is empty deletion not Possible")
             return
-                  
-        if index<0 or index>=self.n: 
-            return IndexError("Index out of bound....deletion not possible")         
-          
-        if index==self.n-1: 
+                 
+        if index<0 or index>=self.n:
+            return IndexError("Index out of bound....deletion not possible")        
+         
+        if index==self.n-1:
             self.A[index]=0
             self.n-=1
             return        
-          
-        for i in range(index,self.n-1): 
-            self.A[i]=self.A[i+1]             
-              
-          
+         
+        for i in range(index,self.n-1):
+            self.A[i]=self.A[i+1]            
+             
+         
         self.A[self.n-1]=0
         self.n-=1
-  
-          
-    def _resize(self, new_cap): 
-        """ 
-        Resize internal array to capacity new_cap 
+ 
+         
+    def _resize(self, new_cap):
         """
-          
-        B = self.make_array(new_cap) # New bigger array 
-          
-        for k in range(self.n): # Reference all existing values 
-            B[k] = self.A[k] 
-              
-        self.A = B # Call A the new bigger array 
-        self.capacity = new_cap # Reset the capacity 
-          
-    def make_array(self, new_cap): 
-        """ 
-        Returns a new array with new_cap capacity 
+        Resize internal array to capacity new_cap
+        """
+         
+        B = self.make_array(new_cap) # New bigger array
+         
+        for k in range(self.n): # Reference all existing values
+            B[k] = self.A[k]
+             
+        self.A = B # Call A the new bigger array
+        self.capacity = new_cap # Reset the capacity
+         
+    def make_array(self, new_cap):
+        """
+        Returns a new array with new_cap capacity
         """
         return (new_cap * ctypes.py_object)()
 
@@ -173,8 +172,8 @@ for multi core processing
 def diff_count(background,current,adc_threshold):
     results = np.abs(np.subtract(background,current))
     counter = np.count_nonzero(results>adc_threshold)
-    return counter 
-    
+    return counter
+   
 def multiprocessing_func(x,y,a):
     diff_count(x,y,a)
 
@@ -210,29 +209,27 @@ if __name__ == "__main__":
 #      entries = range(1024000) # 1 million entries
         results = np.zeros((1280,800)) # prefilled array
         i = 0
-        feature_detect = False 
+        feature_detect = False
         #t_end = time.time()+1
         #while(time.time()<t_end):
          #   try:
         for i in range(max_frames):
             frame = camera.capture(encoding="raw")
             ls[i]=frame.as_array.reshape(1280,800)
-            print(i)
-        
+            #print(i)
+       
         t.start()
         background = ls[0]
         current = ls[1]
         processes = []
-        for i in range(1,10):
-            p = multiprocessing.Process(target=multiprocessing_func, args=(background,current,adc_threshold))
-            processes.append(p)
-            p.start()
-        
+        p = multiprocessing.Process(target=multiprocessing_func, args=(background,current,adc_threshold))
+        processes.append(p)
+        p.start()
         for process in processes:
             process.join()
-        
-        if(process>pix_threshold):
-            feature_detect = True
+       
+        #if(process>pix_threshold):
+        #    feature_detect = True
         t.stop()
         camera.close_camera()
         if(feature_detect):
