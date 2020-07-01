864769b67800d97865d294534fee73b049591af5
shishirb <shishir.bandapalli@hotmail.com>
2020-07-01 11:13:54 -0500

    Started final image capture and processing script

---
+++ DAQ/Cameras/RPi_CameraServers/python/final.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 10:25:56 2020
+
+@author: pi
+"""
+import arducam_mipicamera as arducam
+import v4l2
+import numpy as np
+from PIL import Image
+import time
+#import RPi.GPIO as GPIO
+import ctypes
+from count import count_above
+
+regs = [[0x4F00, 0x01],
+        [0x3030, 0x04],
+        [0x303F, 0x01],
+        [0x302C, 0x00],
+        [0x302F, 0x7F],
+        [0x3823, 0x30],
+        [0x0100, 0x00],]
+
+
+if __name__=="__main__":
+    camera = arducam.mipi_camera()
+    camera.init_camera()
+    
+    print("camera open")
+    camera.set_resolution(1280,800)
+    camera.set_mode(5)
+    print("res set")
+    #for i in range(7):
+        #   camera.write_sensor_reg(regs[i][0],regs[i][1])
+    camera.set_control(v4l2.V4L2_CID_VFLIP, 1)
+    camera.set_control(v4l2.V4L2_CID_HFLIP,1)
+    camera.set_control(v4l2.V4L2_CID_EXPOSURE,4)
+    adc_threshold1 = np.uint8(3)
+    pix_threshold = 199 #15
+    max_frames = 100
+    ls = [None]*100
+    for i in range(100):
+        ls[i] = np.zeros((800,1280),dtype=np.uint8)
+    results = np.zeros((800,1280),dtype=np.uint8)
+    background = np.zeros((800,1280),dtype=np.uint8)
+    current = np.zeros((800,1280),dtype=np.uint8)# prefilled array
+    i = 0
+    feature_detect = False
+    loop=0
+    while(True):
+        try:
+            if(i==100):
+                i = 0
+            else:
+                frame = camera.capture(encoding="raw")
+#                    buff1 = ringBuf[i]
+                ls[i]= frame.as_array.reshape(800,1280)
+                
+                print("capture" +str(i))
+#                    np.copyto(buff1,buff2)
+                if(i==0):
+                    background = ls[0]
+                else:
+                    current=ls[i]
+                    t_start = time.time()
+                    print("this")
+                    np.subtract(background,current,out=results)
+                    print(time.time()-t_start)
+                    t_start=time.time()
+            #        counter = (results>adc_threshold1).sum()
+                    counter1 = count_above(results,adc_threshold1)
+            #        counter1 = (results>adc_threshold1).sum()
+            #        counter2 = (results<adc_threshold2).sum()
+                    print(time.time()-t_start)
+                    counter = counter1
+                    
+                    if(counter>pix_threshold):
+                        break
+                i+=1
+        except KeyboardInterrupt:
+            break
+    camera.close_camera()
+    for i in range(100):
+        im = Image.fromarray(ls[i])
+        im = im.convert("L")
+        im.save("/home/pi/SBCcode/DAQ/Cameras/RPi_CameraServers/python/Captures/"+str(i)+".png")
+        print("images saved")
