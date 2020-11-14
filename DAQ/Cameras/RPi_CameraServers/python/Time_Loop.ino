dcc3436012db17841c3be133b2667e05bbf8c3b5
David V <velascodavid00@gmail.com>
2020-11-13 20:09:03 -0600

    Added structure for timed loop

--- DAQ/Cameras/RPi_CameraServers/python/Time_Loop.ino
+++ DAQ/Cameras/RPi_CameraServers/python/Time_Loop.ino
@@ -1,49 +1,72 @@
-// Scheduling Arduino Code (run every x seconds)
-// http://hwhacks.com/2016/05/08/scheduling-arduino-code-run-every-x-seconds/#:~:text=Arduino%20code%20by%20nature%20runs,halt%20the%20program%20before%20continuing.
-
-byte led = 2;
-
 typedef struct t  {
     unsigned long tStart;
     unsigned long tTimeout;
 };
 
+typedef struct square_wave {
+    unsigned long period;
+    unsigned long phase;
+    unsigned long duty;
+    byte polarity;
+    int counter; //this is what Dr. Dahl called 'clock'
+    int pin;
+    int state;
+};
+
+
+//pin = 37 
+struct square_wave wave1 = {10,2,50,HIGH,0,37,0}; 
+
+
 //Tasks and their Schedules.
-t t_func1 = {0, 1}; //Need one function to run every 10 microseconds.  
-//t t_func2 = {0, 2000}; //Run every 2 seconds. 
+t t_func1 = {0, 10}; 
 
 bool tCheck (struct t *t ) {
-  if (millis() > t->tStart + t->tTimeout) return true;    
+  if (micros() > t->tStart + t->tTimeout) return true;    
 }
 
 void tRun (struct t *t) {
-//change this to micros()    
-    t->tStart = micros();
+  t->tStart = micros();
+}
+
+void update_wave(square_wave wave1) {
+if (wave1.state==0 and wave1.counter > wave1.phase){
+  wave1.state = 1;
+}
+else if (wave1.state==1 and wave1.counter > (wave1.phase + wave1.duty)){
+  wave1.state = 2;
+}
+else if (wave1.state==2 and wave1.counter > wave1.period){
+     wave1.state = 0;
+     wave1.counter = 0;
+}
+
+wave1.counter++;
+
+if (wave1.state==1) {
+  digitalWrite(wave1.pin,wave1.polarity); 
+}
+else { 
+  digitalWrite(wave1.pin, not wave1.polarity); 
+}
+
 }
 
 void setup (void) {
   //Arduino setup.
-  pinMode(led, OUTPUT);
+  pinMode(wave1.pin, OUTPUT);
+ 
 }
 
 void loop (void) {
-    if (tCheck(&t_func1)) {
-      func1();
-      tRun(&t_func1);
+  if (tCheck(&t_func1)) {
+    func1();
+    tRun(&t_func1);
     }
-    
-//    if (tCheck(&t_func2)) {
-//      func2();
-//      tRun(&t_func2);
- //   }
 }
 
+// the square wave lives in here, and eventually so too will the trigger fan in / fan out. 
 void func1 (void) {
-  //This executes every 6s.
-  digitalWrite(led, HIGH); 
-  digitalWrite(led, LOW);
+  update_wave(wave1);
 }
-
-//void func2 (void) {
-  //This executes every 2 seconds.
-//}
+  
