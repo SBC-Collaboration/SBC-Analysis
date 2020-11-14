a07864b5ed05d90a06a3e23c525f694c99095cf4
David V <velascodavid00@gmail.com>
2020-11-13 20:11:25 -0600

    made minor changes to how capture() is implemented

--- DAQ/Cameras/RPi_CameraServers/python/FSIN.py
+++ DAQ/Cameras/RPi_CameraServers/python/FSIN.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 21 17:49:10 2020
+Created on Wed Oct 21 19:46:43 2020
 
 @author: pi
 """
-
-#Working code for capture.py to incorporate GPIO logic and work with Arduino echoing signals
-
 
 #creates .jpg file in Captures folder in SBCcode/Cameras/RPi_CameraServers/python/Captures
 #file is labelled with data and time when taken
@@ -15,9 +12,6 @@ Created on Wed Oct 21 17:49:10 2020
 import arducam_mipicamera as arducam
 import v4l2
 from datetime import datetime
-import RPi.GPIO as GPIO
-import threading
-
 
 regs = [[0x4F00, 0x01],
         [0x3030, 0x04],
@@ -28,17 +22,12 @@ regs = [[0x4F00, 0x01],
         [0x0100, 0x00],]
 
 
-
-        
-
-
 def set_controls(camera):
     try:
         print("Enable Auto Exposure...")
         camera.software_auto_exposure(enable = True)
     except Exception as e:
         print(e)
-
 def capture(camera):
     frame = camera.capture(encoding = 'jpeg')
     d1 = datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
@@ -47,41 +36,27 @@ def capture(camera):
     frame.as_array.tofile(path)
             #Remove frame from memory
     del frame
-
-def main():
-#    thread = threading.Thread(target=wait_sig)
-#    thread.start()
-#    thread.join()
-    camera = arducam.mipi_camera()
-    print("Open camera...")
-    camera.init_camera()
-    print("Setting the resolution...")
-    fmt = camera.set_resolution(12800, 800)
-    print("res set")
-    for i in range(7):
-        camera.write_sensor_reg(regs[i][0],regs[i][1])
-    camera.set_control(v4l2.V4L2_CID_VFLIP, 1)
-    camera.set_control(v4l2.V4L2_CID_HFLIP,1)
-    camera.set_control(v4l2.V4L2_CID_EXPOSURE,1)
-    print("Current resolution is {}".format(fmt))
-    set_controls(camera)
-    capture(camera)
-#           sleep(1)
-#            capture(camera)
-#    for i in range(100):
-#        capture(camera)
-#        print(i)
-    print("Close camera...")
-    camera.close_camera()
-      
     
-    
-
-         
-if __name__ == "__main__":
-    try:
-        main()
-    except Exception as e:
-        print(e)
-        
             
+if __name__ == "__main__":
+        try:
+            camera = arducam.mipi_camera()
+            print("Open camera...")
+            camera.init_camera()
+            print("Setting the resolution...")
+            fmt = camera.set_resolution(1920, 1080)
+            camera.set_mode(5)
+            camera.set_control(v4l2.V4L2_CID_VFLIP, 1)
+            camera.set_control(v4l2.V4L2_CID_HFLIP,1)
+            camera.set_control(v4l2.V4L2_CID_EXPOSURE,1)
+            print("Current resolution is {}".format(fmt))
+            for i in range(7):
+                camera.write_sensor_reg(regs[i][0],regs[i][1])
+            try:
+                capture(camera)  
+            except KeyboardInterrupt:
+                print('Interrupted')      
+            print("Close camera...")
+            camera.close_camera()
+        except Exception as e:
+            print(e)
